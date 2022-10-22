19028695b680b85063f19069109344e5fa5ad3c6
Jan Krueger <jankrueger1999@gmail.com>
2022-10-22 21:09:28 +0200

    feat(praktikum): add brightness support

--- praktikum/lib/src/app.dart
+++ praktikum/lib/src/app.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import 'package:praktikum/src/widgets/tabbar.dart';
 
-class DatenkrakeApp extends StatelessWidget {
+class DatenkrakeApp extends StatefulWidget {
   const DatenkrakeApp({super.key});
+
+  @override
+  DatenkrakeAppState createState() => DatenkrakeAppState();
+}
+
+class DatenkrakeAppState extends State<DatenkrakeApp>
+    with WidgetsBindingObserver {
+  Brightness brightness = Brightness.light;
+
+  @override
+  void initState() {
+    super.initState();
+    // Get the current brightness
+
+    brightness = WidgetsBinding.instance.window.platformBrightness;
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    setState(() {
+      brightness = WidgetsBinding.instance.window.platformBrightness;
+    });
+
+    // Log the brightness change
+    print('Brightness changed to: $brightness');
+
+    super.didChangePlatformBrightness();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
         title: 'Datenkrake',
-        theme: CupertinoThemeData(brightness: Brightness.dark),
-        home: TabbarScreen());
+        theme: CupertinoThemeData(
+          brightness: brightness,
+        ),
+        home: const TabbarScreen());
   }
 }
