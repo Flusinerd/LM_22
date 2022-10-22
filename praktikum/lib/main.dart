be2066f16d53cbddc67f1d09333823eb1a376f49
Jan Krueger <jankrueger1999@gmail.com>
2022-10-22 19:27:54 +0200

    refactor(praktikum): move sidebar to own widget

--- praktikum/lib/main.dart
+++ praktikum/lib/main.dart
@@ -37,6 +37,51 @@ class _MyHomePageState extends State<MyHomePage> {
       body: const Center(
         child: Text("Hallo Welt"),
       ),
+      drawer: const SidebarDrawer()
     );
+  }
+}
+
+class SidebarDrawer extends StatefulWidget {
+  const SidebarDrawer({super.key});
+
+  @override
+  State<SidebarDrawer> createState() => _SidebarDrawerState();
+}
+
+class _SidebarDrawerState extends State<SidebarDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
   }
 }
