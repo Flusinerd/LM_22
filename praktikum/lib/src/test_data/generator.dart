4c55c3fba275611add28298d354131565a2617e9
JasminBreemann <73950301+JasminBreemann@users.noreply.github.com>
2022-11-02 19:53:01 +0100

    added gyroscope and accelerometer baseline

--- praktikum/lib/src/test_data/generator.dart
+++ praktikum/lib/src/test_data/generator.dart
@@ -1,12 +1,18 @@
+import 'dart:ffi';
+
 import 'package:meta/meta.dart';
 import 'dart:async';
 import 'dart:math' as math;
+import 'package:sensors_plus/sensors_plus.dart';
 
 class SensorData {
   String name;
   double delay = 1;
   bool isRunning = true;
   final Function(String) onDataChanged;
+  double x = 0.0;
+  double y = 0.0;
+  double z = 0.0;
 
   SensorData({
     required this.name,
@@ -23,6 +29,23 @@ class SensorData {
   }
 
   String getData(Timer timer) {
-    return ('{"name": "${this.name}", "timestamp": "${DateTime.now()}", "value":${(math.Random().nextDouble() * 50)}}');
+    gyroscoping();
+    return ('{"name": "${this.name}", "timestamp": "${DateTime.now()}", "value":$x}');
+  }
+
+  void acceleration() {
+    userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      x = event.x;
+      y = event.y;
+      z = event.z;
+    });
+  }
+
+  void gyroscoping() {
+    gyroscopeEvents.listen((event) {
+      x = event.x;
+      y = event.y;
+      z = event.z;
+    });
   }
 }
